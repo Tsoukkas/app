417a39f4bb2e0ffb41aafac8330b3e081011d521
unknown <antonis.t@citizensinpower.org>
2026-02-18 15:28:57 +0200

    Force app to always use light theme

--- hooks/use-color-scheme.web.ts
+++ hooks/use-color-scheme.web.ts
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+/*
+ import { useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
- */
+ */ /*
 export function useColorScheme() {
   const [hasHydrated, setHasHydrated] = useState(false);
 
@@ -18,4 +19,8 @@ export function useColorScheme() {
   }
 
   return 'light';
+}
+  */
+export function useColorScheme() {
+  return "light" as const;
 }
